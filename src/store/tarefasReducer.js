ca108c20191f829e80399b89b1a0448375d2ceef
yago-codev <yagomilano92@gmail.com>
2020-05-01 18:31:51 -0300

    Action creator de listagem de tarefas

--- src/store/tarefasReducer.js
+++ src/store/tarefasReducer.js
@@ -1,3 +1,5 @@
+import api from '../services/api';
+
 const ACTIONS = {
   LISTAR: 'TAREFAS_LISTAR',
   ADICIONAR: 'TAREFAS_ADICIONAR',
@@ -15,5 +17,20 @@ export const tarefasReducer = (state = ESTADO_INICIAL, action) => {
     
     default:
       return { state }
+  }
+}
+
+export function listar() {
+  return dispatch => {  // dispatch do 'redux-thunk'
+    const email = localStorage.getItem('@GerenciadorTarefas:email_usuario_logado');
+    const headers = { 'x-tenant-id' : email }
+  
+    api.get('/tarefas', { headers })
+      .then(response => {
+        dispatch({
+          type: ACTIONS.LISTAR,
+          tarefas: response.data,
+        });
+      })
   }
 }
